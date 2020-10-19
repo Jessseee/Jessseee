188a417dcebf909171eaff6b67e5431124b582e4
Jessseee <jssvssr2000@gmail.com>
2020-10-19 17:15:13 +0200

    Added dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,32 @@
+/* PLAY BUTTON */
 function playButton(video, overlay) {
     if(video.paused) {
         video.play()
-        overlay.style.display = "none";
+        overlay.style.display = 'none';
     } else {
         video.pause();
-        overlay.style.display = "block";
+        overlay.style.display = 'block';
     }
 }
+
+
+/* THEMES */
+const themeToggle = document.querySelector("#theme-toggle");
+
+if(getCookie('theme') === 'dark') themeToggle.checked = true;
+
+function toggleTheme(e) {
+    document.body.style.transition = 'background-color 300ms ease-in-out, color 300ms ease-in-out';
+
+    if (e.target.checked) {
+        setCookie('theme', "dark", 360)
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        setCookie('theme', "light", 360)
+
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+
+themeToggle.addEventListener('change', toggleTheme, false);

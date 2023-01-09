12b710a66b99ef7aeaeaa6a1ca99ff686a4a987c
jessseee <jssvssr2000@gmail.com>
2023-01-09 21:55:43 +0100

    Reverted fix for animating elements on first load

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,13 @@
 <script>
   import Navbar from '$lib/components/Navbar.svelte';
   import '$lib/styles/global.sass';
-  import { onMount } from 'svelte';
-
-  let loaded;
-  onMount(() => (loaded = true));
 </script>
 
-{#if loaded}
-  <Navbar />
+<Navbar />
 
-  <main>
-    <slot />
-  </main>
-{/if}
+<main>
+  <slot />
+</main>
 
 <style lang="sass">
   @import "$lib/styles/variables.sass"
